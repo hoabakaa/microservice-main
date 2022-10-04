937d804a64e4f187e24774684c6c80114dc4464d
Harusora <longcnhe141293@gmail.com>
2022-10-05 00:51:59 +0700

    root

---
+++ inventory-service/src/main/java/com/caonhatlong/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,15 @@
+package com.caonhatlong.inventoryservice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/inventory")
+public class InventoryController {
+
+    @GetMapping("/{sku-code}")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@PathVariable("sku-code") String skuCode){
+        return true;
+    }
+}
---
+++ inventory-service/src/main/java/com/caonhatlong/inventoryservice/service/InventoryService.java
@@ -0,0 +1,6 @@
+package com.caonhatlong.inventoryservice.service;
+
+public interface InventoryService {
+
+    public boolean isInStock(String skuCode);
+}
